5c80703706785b9ae1c2c0b402914b504b02552f
yeondae-0524 <qazxx12345@pusan.ac.kr>
2026-05-19 16:00:02 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const smallGroupBtn = document.getElementById('small-group');
+const largeGroupBtn = document.getElementById('large-group');
+
+smallGroupBtn.addEventListener('click', function() {
+    alert("2~3인용 소수정예 게임 시작 준비 완료!");
+});
+
+largeGroupBtn.addEventListener('click', function() {
+    alert("4인 이상 텐션업 게임 시작 준비 완료!");
+});
